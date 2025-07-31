d7d8d3d07320f36a51efb2d8b032637a5753a00e
Wasem1209 <philipwasem3@gmail.com>
2025-07-31 19:32:14 +0100

    footer change again

--- gensto-frontend/src/app/layout.tsx
+++ gensto-frontend/src/app/layout.tsx
@@ -15,7 +15,7 @@ export const metadata = {
 export default function RootLayout({ children } : {children:  ReactNode}){
   return(
     <html lang="en">
-      <body ><Header /> <main>{children}</main><Footer /></body>
+      <body ><Header />{children}<Footer /></body>
      
     </html>
   )
