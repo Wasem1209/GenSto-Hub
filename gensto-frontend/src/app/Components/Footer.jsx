fce6477984ab5d11b67b475a03cbcb23b36e0062
Wasem1209 <philipwasem3@gmail.com>
2025-07-31 19:25:05 +0100

    footer change

--- gensto-frontend/src/app/Components/Footer.jsx
+++ gensto-frontend/src/app/Components/Footer.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { SocialIcon } from 'react-social-icons';
 
-export default function footer() {
+export default function Footer() {
   return (
     <footer className="bg-gray-900 text-white py-10 px-6">
       <div className="max-w-7xl mx-auto grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8">
